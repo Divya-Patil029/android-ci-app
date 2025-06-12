3808938a9deb89dcef24c141ac70bef71398588c
Divya-Patil029 <divyapatil1176@gmail.com>
2025-06-12 14:49:53 +0530

    updated from updated jenkins to sir from

--- app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
+++ app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello jenkins updated $name!",
+        text = "Hello sir from  $name!",
         modifier = modifier
     )
 }
