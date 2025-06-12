f6e59dff58a82685d42eacdf9360b22af9627658
Divya-Patil029 <divyapatil1176@gmail.com>
2025-06-12 12:35:52 +0530

    updated from intern to jenkins

--- app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
+++ app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hi intern $name!",
+        text = "Hello jenkins $name!",
         modifier = modifier
     )
 }
