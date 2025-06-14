caf170e3caa34a0337d59b3f6b5d62ab81a84a5e
Divya-Patil029 <divyapatil1176@gmail.com>
2025-06-14 16:34:05 +0530

    updated from guys to friends

--- app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
+++ app/src/main/java/com/boatdroid/android_ci_app/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello guys from  $name!",
+        text = "Hello friends from  $name!",
         modifier = modifier
     )
 }
